990897fa2e3576740b4d7094618ff8dab8a62859
Fernando Quintanilla <df.quintanillah@gmail.com>
2023-03-16 21:19:53 -0600

    Comentario en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Timer from './components/timer/timer'
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState('var(--first-color)')
-
   return (
     <div>
       <Timer />
